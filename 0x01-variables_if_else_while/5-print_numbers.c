f00b09190e51a05eace8f79c2970c4fa0c7d34d5
Oussema3 <ldrajmioussema2@gmail.com>
2020-09-17 17:43:48 +0000

    singleNumbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Return: 0 in success
+ */
+int main(void)
+{
+int a;
+for (a = 0 ; a < 10 ; a++)
+
+putchar(a);
+
+putchar('\n');
+return (0);
+}
