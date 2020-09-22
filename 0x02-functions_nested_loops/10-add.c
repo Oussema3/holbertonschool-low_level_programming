2fddb9ef763ba592c5ddea17e3edeb55810d18e5
Oussema3 <ldrajmioussema2@gmail.com>
2020-09-22 10:33:44 +0000

    addiction

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,6 +1,13 @@
 #include "holberton.h"
-int add(int a, int b){
-  a+=b;
-  return (a);
+/**
+ * add - add 2 numbers
+ * @b: second num to add to a
+ * @a: first number
+ * Return: 1 or 0 success
+ */
+int add(int a, int b)
+{
+a += b;
+return (a);
 }
 
