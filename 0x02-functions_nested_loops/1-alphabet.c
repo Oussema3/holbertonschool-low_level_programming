86b7b050bf54a77b36317225cd8109e54d621139
Oussema3 <ldrajmioussema2@gmail.com>
2020-09-22 08:26:13 +0000

    alphabet

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,20 +1,20 @@
 #include "holberton.h"
 /**
- * main - entry point
+ * print_alphabet - entry point
  *
  * Return: 0 in success
  */
 void print_alphabet(void)
 {
-  int a;
+int a;
 
-  a = 'a';
-  while (a <= 'z')
-    {
-      _putchar(a++);
-    }
-  _putchar('\n');
+a = 'a';
+while (a <= 'z')
+{
+_putchar(a++);
+}
+_putchar('\n');
 
-  return;
+return;
 
 }
