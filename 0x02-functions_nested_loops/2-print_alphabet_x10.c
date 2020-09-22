4649b3737f5253c7e72a4da93948d51d0c842639
Oussema3 <ldrajmioussema2@gmail.com>
2020-09-22 08:36:11 +0000

    10alphabets

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,20 +1,23 @@
 #include "holberton.h"
+/**
+ * print_alphabet_x10 - return the alphabet while a condition is true
+ *
+ *
+ * Return: 1 or 0 success
+ */
 void print_alphabet_x10(void)
-{ int i;
-  int j;
-  i = 0;
-  while (i < 10) {
-    
-    j ='a';
-    while (j <= 'z')
 {
-      
-   _putchar(j++);
-    }
-
-  
-    _putchar('\n');
-    i++; 
-  
-  }
+int i;
+int j;
+i = 0;
+while (i < 10)
+{
+j = 'a';
+while (j <= 'z')
+{
+_putchar(j++);
+}
+_putchar('\n');
+i++;
+}
 }
