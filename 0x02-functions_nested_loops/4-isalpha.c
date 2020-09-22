b428d13c55a5e386a089e2f5b8cea212cd25e95c
Oussema3 <ldrajmioussema2@gmail.com>
2020-09-22 01:03:17 +0000

    alpha

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,4 +1,10 @@
 #include "holberton.h"
+/**
+ * _isalpha - returns 1 if  the number is positve else it will return 0
+ *
+ * @c: show alphabet character
+ * Return: 1 or 0 success
+ */
 int _isalpha(int c)
 {
 if ((c >= 97 && c <= 122) || (c >= 65 && c <= 90))
