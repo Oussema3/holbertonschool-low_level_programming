046e7d1fce7322170116cd7e701df6ff61d2a9f6
Oussema3 <ldrajmioussema2@gmail.com>
2020-09-24 10:10:15 +0000

    header

--- 0x04-more_functions_nested_loops/holberton.h
+++ 0x04-more_functions_nested_loops/holberton.h
@@ -1,5 +1,14 @@
 #ifndef HOLBERTON_H
 #define HOLBERTON_H
+int mul(int a, int b);
+void print_numbers(void);
+void print_most_numbers(void);
+void more_numbers(void);
+void print_line(int n);
+void print_diagonal(int n);
+void print_square(int size);
+void print_triangle(int size);
+int _isdigit(int c);
 void print_times_table(int n);
 void print_to_98(int n);
 void times_table(void);
