fd9de60d89218a5ae70e7b85142dc3fc5f38869c
Oussema3 <ldrajmioussema2@gmail.com>
2020-09-24 21:57:58 +0000

    primes

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -1,1 +1,19 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <math.h>
+/**
+ * main - Gives the largest prime factor of a giving number
+ *
+ * Return: returns
+ */
+int main(void)
+{
+long size = 612852475143;
+long i;
+for (i = 2; i < sqrt(size); i++)
+while ((size % i) == 0)
+size /= i;
+printf("%ld\n", size);
 
+return (0);
+}
