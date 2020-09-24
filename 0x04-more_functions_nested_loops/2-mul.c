3052278ca5ec1771bc03c93c146680e7628259cf
Oussema3 <ldrajmioussema2@gmail.com>
2020-09-24 10:24:27 +0000

    multiplication

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -2,12 +2,12 @@
 /**
  * mul - checks if c is digit between 0 and 9 or not
  * @a: first integer
- * @b: second integer 
- * Return: n  the multiplication value 
+ * @b: second integer
+ * Return: n  the multiplication value
  */
 int mul(int a, int b)
 {
-  int n;
-  n= a*b;
-  return(n);
-    }
+int n;
+n = a * b;
+return (n);
+}
