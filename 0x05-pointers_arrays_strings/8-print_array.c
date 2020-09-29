40fb81053d86824d7dbca5834e0b72b3179730f9
Oussema3 <ldrajmioussema2@gmail.com>
2020-09-29 21:53:27 +0000

    Arrayss

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ *print_array - print array
+ *@a: array
+ *@n: integer
+ */
+void print_array(int *a, int n)
+{
+int a;
+for (a = 0; a < n; a++)
+{
+printf("%d", a[a]);
+if (a == n - 1)
+continue;
+printf(", ");
+}
+printf("\n");
+}
