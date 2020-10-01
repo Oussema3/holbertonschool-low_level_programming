11830b65b29e7cf75a484c6502acc2065a4afd18
Oussema3 <ldrajmioussema2@gmail.com>
2020-10-01 18:10:26 +0000

    ReverseArray

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -2,13 +2,13 @@
 #include "holberton.h"
 /**
  *reverse_array - reverse array
- *@a: pointer to char
+ *@a: array
  *@n: number of element in array
  *Return: array reveersed
  */
 void reverse_array(int *a, int n)
 {
-  int i ,b;
+int i, b;
 i = 0;
 
 for (i = 0; i < n / 2; ++i)
