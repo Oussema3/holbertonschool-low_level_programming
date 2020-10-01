077a60a95cf1b235f5c5663f2086ed4b142bbbab
Oussema3 <ldrajmioussema2@gmail.com>
2020-10-01 20:52:20 +0000

    RotScripting

--- 0x06-pointers_arrays_strings/8-rot13.c
+++ 0x06-pointers_arrays_strings/8-rot13.c
@@ -1,25 +1,25 @@
 #include <stdio.h>
 #include "holberton.h"
 /**
- *leet - translate to leet discussion
+ **rot13 - translate to leet discussion
  *@s: pointer of result
  *Return: string
  */
 char *rot13(char *s)
 {
-  int x = 0, y;
-  char d[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
-  char c[] = "nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM";
-  while (s[x] != '\0')
-    {
-      for (y = 0; d[y] != '\0'; y++)
-	{
-	  if (s[x] == d[y] || s[x] == d[y])
-	    {
-	      s[x] = c[y];
-	    }
-	}
-      x++;
-    }
-  return (s);
+int x = 0, y;
+char d[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+char c[] = "nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM";
+while (s[x] != '\0')
+{
+for (y = 0; d[y] != '\0'; y++)
+{
+if (s[x] == d[y] || s[x] == d[y])
+{
+s[x] = c[y];
+}
+}
+x++;
+}
+return (s);
 }
