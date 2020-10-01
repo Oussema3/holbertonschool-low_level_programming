40664f99eee7404d08070ebb5fb1aa2afd77f732
Oussema3 <ldrajmioussema2@gmail.com>
2020-10-01 08:56:13 +0000

    concating

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+#include <string.h>
+/**
+ *_strcat - concate 2 strings
+ *@dest: the first string
+ *@src: the second string
+ *Return: string
+ */
+char *_strcat(char *dest, char *src)
+{
+
+char *test = strcat(dest, src);
+return (test);
+}
