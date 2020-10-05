f4f55c6ede607fec346286a0b35f5110b92585e4
Oussema3 <ldrajmioussema2@gmail.com>
2020-10-05 13:49:18 +0000

    Kasparov

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+/**
+ *print_chessboard - print chess board using characters
+ *@a: var
+ * Return: F.diagram
+ */
+void print_chessboard(char (*a)[8])
+{
+int i = 0;
+int j = 0;
+
+while (i < 8)
+{
+j = 0;
+while (j < 8)
+{
+putchar(a[i][j]);
+j++;
+}
+i++;
+putchar('\n');
+}
+}
