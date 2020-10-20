afeb24d4a6a9acdae0fee7fbcf74a0db654d2d0c
Oussema3 <ldrajmioussema2@gmail.com>
2020-10-20 04:56:04 +0000

    Alertgit add 2-print_dog.c Hard Coding

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -12,7 +12,7 @@ return;
 if (d->name == NULL)
 printf("Name: (nil)\n");
 else
-  printf("Name: %s\n", d->name);
+printf("Name: %s\n", d->name);
 
 printf("Age: %f\n", d->age);
 if (d->owner == NULL)
