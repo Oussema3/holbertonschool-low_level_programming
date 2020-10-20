0354a9efe39c24c50d12105ad3f8a3b3889631e4
Oussema3 <ldrajmioussema2@gmail.com>
2020-10-20 05:10:24 +0000

    Header

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -13,7 +13,8 @@ char *name;
 float age;
 char *owner;
 };
-dog_t *new_dog(char *name, float age, char *owner);
 void print_dog(struct dog *d);
 void init_dog(struct dog *d, char *name, float age, char *owner);
+typedef struct dog dog_t;
+dog_t *pt_to_stuct(char *name, float age, char *owner);
 #endif /* DOG */
