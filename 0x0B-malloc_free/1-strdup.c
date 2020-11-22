6c942c0829208358707235598498c34b557bc4bb
Oussema3 <ldrajmioussema2@gmail.com>
2020-11-22 21:32:38 +0000

    no wings without imagination

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -1,34 +1,30 @@
 #include <stdlib.h>
+#include "holberton.h"
+
+
 /**
- *length - give us length of a string
- *@L: string to calculate it's length
- *Return: obviously the lenght of the string
- */
-int length(char *L)
-{
-int i = 0;
-while (L[i] != '\0')
-i++;
-return (i);
-}
-/**
- *_strdup - copies the string in the allocated memory
- *@str: string
- *Return: string'
+ * _strdup - returns pointer to an allocated space in me!mory
+ *@str: pointer
+ * Return: pointer to a string
  */
 char *_strdup(char *str)
 {
-
-char *strcpd;
-int a;
-
-if (str == NULL)
-return (NULL);
-strcpd = malloc(sizeof(char) * length(str) + 1);
-if (strcpd == NULL)
-return (NULL);
-for (a = 0; a <= length(str); a++)
-strcpd[a] = str[a];
-strcpd[a + 1] = '\0';
-return (strcpd);
+int i, j;
+char *round;
+if (str == '\0')
+{
+return (0);
 }
+for (j = 0; str[j] != '\0';)
+{
+j++;
+}
+round = malloc(sizeof(char) * j + 1);
+if (round == '\0')
+return (0);
+for (i = 0; i < j; i++)
+round[i] = str[i];
+round[j] = str[j];
+return (round);
+}
+
