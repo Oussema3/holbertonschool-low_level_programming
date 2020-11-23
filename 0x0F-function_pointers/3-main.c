e7a32877d39c935d5e789838f0abf0f4941e9ff8
Oussema3 <ldrajmioussema2@gmail.com>
2020-11-23 03:40:16 +0000

    Please checker

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -9,22 +9,27 @@
  */
 int main(int argc, char **argv)
 {
-int res, n1, n2;
+int n1, n2;
 int (*operator)(int, int);
 if (argc != 4)
 {
 printf("Error\n");
 exit(98);
 }
-n1 = atoi(argv[1]);
-n2 = atoi(argv[3]);
 operator = get_op_func(argv[2]);
 if (operator == NULL)
 {
 printf("Error\n");
 exit(99);
 }
-res = operator(n1, n2);
-printf("%d\n", res);
+if (argv[2][1] != '\0')
+{
+printf("Error\n");
+exit(99);
+}
+n1 = atoi(argv[1]);
+n2 = atoi(argv[3])
+
+printf("%d\n",operator(n1,n2));
 return (0);
 }
