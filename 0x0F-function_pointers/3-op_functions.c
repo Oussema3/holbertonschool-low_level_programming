4dee309415438b5229feb3736d22f0f9582a3d4b
Oussema3 <ldrajmioussema2@gmail.com>
2020-11-23 03:24:07 +0000

    calc

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,67 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "3-calc.h"
+/**
+ * op_mul - multiply 2 numbers
+ *@a: first num
+ *@b: second num
+ *Return: int
+ */
+int op_mul(int a, int b)
+{
+return (a * b);
+}
+
+
+/**
+ * op_div - make devision between two numbers
+ *@a: first num
+ *@b: second num
+ *Return: int
+ */
+int op_div(int a, int b)
+{
+if (b == 0)
+{
+printf("Error\n");
+exit(100);
+}
+else
+return (a / b);
+}
+/**
+ * op_mod - determpines the remaining of the division between 2 numbers
+ *@a: first num
+ *@b: second num
+ *Return: int
+ */
+int op_mod(int a, int b)
+{
+if (b == 0)
+{
+printf("Error\n");
+exit(100);
+}
+else
+return (a % b);
+}
+/**
+ * op_add - addition of 2 numbers
+ *@a: first num
+ *@b: second num
+ *Return: int
+ */
+int op_add(int a, int b)
+{
+return (a + b);
+}
+/**
+ * op_sub - subtitute 2 numbers
+ *@a: first num
+ *@b: second num
+ *Return: int
+ */
+int op_sub(int a, int b)
+{
+return (a - b);
+}
