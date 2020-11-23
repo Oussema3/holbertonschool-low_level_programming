23769175e8085e2af3ddb3791910b888cf5f7fb8
Oussema3 <ldrajmioussema2@gmail.com>
2020-11-23 01:40:24 +0000

    After 19 wrong compilations

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -1,9 +1,9 @@
 #include "function_pointers.h"
 /**
- * array_iterator - executes a function as a parameter 
+ * array_iterator - executes a function as a parameter
  * @array: array
  * @size: size
- * @action: nice name 
+ * @action: nice name
  * Return: void
  */
 void array_iterator(int *array, size_t size, void (*action)(int))
