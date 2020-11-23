2d41740bd07a5e9ff198ea93f5b00063105f07f2
Oussema3 <ldrajmioussema2@gmail.com>
2020-11-23 15:55:39 +0000

    Header

--- 0x14-bit_manipulation/holberton.h
+++ 0x14-bit_manipulation/holberton.h
@@ -1,4 +1,11 @@
 #ifndef HOLBERTON_H
 #define HOLBERTON_H
+#include <stdio.h>
+#include <stdlib.h>
 unsigned int binary_to_uint(const char *b);
+int _putchar(char c);
+void print_binary(unsigned long int n);
+
+
+
 #endif
