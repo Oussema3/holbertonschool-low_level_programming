2714cbdcb73542b09d10e33bf26d6b2cccf41554
Oussema3 <ldrajmioussema2@gmail.com>
2020-11-23 03:51:50 +0000

    Pls checker

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -1,36 +1,31 @@
-#include <stdio.h>
 #include <stdlib.h>
+#include <stdio.h>
 #include "3-calc.h"
-#include <string.h>
 /**
- * main - print res
+ * main - print calc
  * @argc: argc
  * @argv: argv
  * Return: int
  */
 int main(int argc, char **argv)
 {
-int n1, n2;
+int res, n1, n2;
 int (*operator)(int, int);
 if (argc != 4)
 {
 printf("Error\n");
 exit(98);
 }
+n1 = atoi(argv[1]);
+n2 = atoi(argv[3]);
 operator = get_op_func(argv[2]);
+
 if (operator == NULL)
 {
 printf("Error\n");
 exit(99);
 }
-if (argv[2][1] != '\0')
-{
-printf("Error\n");
-exit(99);
-}
-n1 = atoi(argv[1]);
-n2 = atoi(argv[3])
-
-printf("%d\n",operator(n1,n2));
+res = operator(n1, n2);
+printf("%d\n", res);
 return (0);
 }
