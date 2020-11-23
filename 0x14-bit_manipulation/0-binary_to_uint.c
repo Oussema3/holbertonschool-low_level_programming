0dbe530e130572acaac734ff7fa3e4ef7be555a8
Oussema3 <ldrajmioussema2@gmail.com>
2020-11-23 15:24:37 +0000

    repère orthonormé OIJ

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,26 @@
+#include "holberton.h"
+/**
+ *binary_to_uint - convert binary to int
+ *@b: str
+ *Return: unsigned int
+ */
+unsigned int binary_to_uint(const char *b)
+{
+unsigned int o = 0, i = 1;
+int j = 0;
+
+if (*b == '\0')
+return (0);
+while (b[j] != '\0')
+j++;
+j -= 1;
+while (j >= 0)
+{
+if (b[j] != '0' && b[j] != '1')
+return (0);
+o = o + ((b[j] - '0') * i);
+i *= 2;
+j--;
+}
+return (o);
+}
