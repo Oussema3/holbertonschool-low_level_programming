1be209fe4b299ccac3ecf2cd121d5c14d411f441
Oussema3 <ldrajmioussema2@gmail.com>
2021-03-08 16:41:15 +0000

    fixing betty style

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -6,14 +6,14 @@
  */
 size_t print_dlistint(const dlistint_t *h)
 {
-  size_t i;
+size_t i;
 
-  if (h == NULL)
-    return (0);
-  for (i = 0; h != NULL; i++)
-    {
-      printf("%d\n", h->n);
-      h = h->next;
-    }
-  return (i);
+if (h == NULL)
+return (0);
+for (i = 0; h != NULL; i++)
+{
+printf("%d\n", h->n);
+h = h->next;
+}
+return (i);
 }
